9a0f05b251a6c61859bced8c14dc1406a2e00399
Nabelaldwese <Nbelaldwese1@gmail.com>
2025-11-25 15:50:30 +0000

    fix

--- python-hello_world/6-concat.py
+++ python-hello_world/6-concat.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
-str1 = "Battery"
-str2 = "School"
-print(f"Welcome to {str1} {str2}!")
-str2 = "Street"
-print(f"Welcome to {str1} {str2}!")
+str1 = "Holberton"
+str2 = "School!"
+str1 = "Welcome to " + str1 + " " + str2
+print(str1)
