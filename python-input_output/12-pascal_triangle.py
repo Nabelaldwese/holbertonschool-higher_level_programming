e04441463cf806975fb5b33c9d68609466803246
Nabelaldwese <Nbelaldwese1@gmail.com>
2025-12-22 14:19:08 +0000

    Python Input/Output: add Pascal triangle

---
+++ python-input_output/12-pascal_triangle.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""Module that provides a function to generate Pascal's triangle."""
+
+
+def pascal_triangle(n):
+    """Return a list of lists representing Pascal's triangle of n.
+
+    If n <= 0, return an empty list.
+    """
+    if n <= 0:
+        return []
+
+    triangle = []
+
+    for i in range(n):
+        row = [1] * (i + 1)
+        for j in range(1, i):
+            row[j] = triangle[i - 1][j - 1] + triangle[i - 1][j]
+        triangle.append(row)
+
+    return triangle
