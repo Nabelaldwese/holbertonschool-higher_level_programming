c1c4ec9573d254d608442ee351657e466c865473
Nabelaldwese <Nbelaldwese1@gmail.com>
2025-12-22 14:16:15 +0000

    Python Input/Output: add Student to_json with filter

---
+++ python-input_output/10-student.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""Module that defines a Student class with JSON filtering."""
+
+
+class Student:
+    """Define a student with basic attributes."""
+
+    def __init__(self, first_name, last_name, age):
+        """Initialize a Student instance."""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """Return a dictionary representation of a Student instance.
+
+        If attrs is a list of strings, only attributes in the list
+        are included. Otherwise, all attributes are returned.
+        """
+        if isinstance(attrs, list):
+            result = {}
+            for attr in attrs:
+                if isinstance(attr, str) and hasattr(self, attr):
+                    result[attr] = getattr(self, attr)
+            return result
+        return self.__dict__
