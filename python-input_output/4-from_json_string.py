eb378ae693359fbb30b07e4adaa307de7e85e024
Nabelaldwese <Nbelaldwese1@gmail.com>
2025-12-22 14:03:50 +0000

    Python Input/Output: add from_json_string function

---
+++ python-input_output/4-from_json_string.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""Module for JSON deserialization."""
+import json
+
+
+def from_json_string(my_str):
+    """Return a Python object represented by a JSON string."""
+    return json.loads(my_str)
