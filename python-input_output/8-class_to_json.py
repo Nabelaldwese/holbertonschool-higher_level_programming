c6dd64896127753715c6c0a853de0a4e84e807cf
Nabelaldwese <Nbelaldwese1@gmail.com>
2025-12-22 14:13:34 +0000

    Fix PEP8 line length in class_to_json

--- python-input_output/8-class_to_json.py
+++ python-input_output/8-class_to_json.py
@@ -3,5 +3,5 @@
 
 
 def class_to_json(obj):
-    """Return the dictionary description of an object for JSON serialization."""
+    """Return a dictionary representation of an object."""
     return obj.__dict__
