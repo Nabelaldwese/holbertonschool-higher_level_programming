cbe3f9fe2effba932ae88107d97dc01d6863e6a9
Nabelaldwese <Nbelaldwese1@gmail.com>
2026-01-12 10:11:32 +0000

    Get state by name using SQLAlchemy

---
+++ python-object_relational_mapping/10-model_state_my_get.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+"""
+Prints the id of the State object with the given name.
+"""
+
+import sys
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
+
+
+def main():
+    """Connects to the database and prints the id of the given state."""
+    username = sys.argv[1]
+    password = sys.argv[2]
+    db_name = sys.argv[3]
+    state_name = sys.argv[4]
+
+    engine = create_engine(
+        "mysql+mysqldb://{}:{}@localhost/{}".format(
+            username, password, db_name
+        ),
+        pool_pre_ping=True
+    )
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    state = session.query(State).filter(State.name == state_name).first()
+
+    if state is None:
+        print("Not found")
+    else:
+        print(state.id)
+
+    session.close()
+
+
+if __name__ == "__main__":
+    main()
