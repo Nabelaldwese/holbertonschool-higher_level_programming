d1063a58e9bebd628648dc736b9126de517f3c99
Nabelaldwese <Nbelaldwese1@gmail.com>
2026-01-12 09:51:13 +0000

    Safe filter states against SQL injection

---
+++ python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+"""
+Displays all states where name matches the user input.
+Safe from MySQL injection.
+"""
+
+import sys
+import MySQLdb
+
+
+def main():
+    """Connects to MySQL and prints matching states safely."""
+    username = sys.argv[1]
+    password = sys.argv[2]
+    db_name = sys.argv[3]
+    state_name = sys.argv[4]
+
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=username,
+        passwd=password,
+        db=db_name
+    )
+    cur = db.cursor()
+
+    cur.execute(
+        "SELECT * FROM states WHERE name = %s ORDER BY id ASC;",
+        (state_name,)
+    )
+
+    for row in cur.fetchall():
+        print(row)
+
+    cur.close()
+    db.close()
+
+
+if __name__ == "__main__":
+    main()
