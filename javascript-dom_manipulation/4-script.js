42bc31b1076614f39f86d6824cfdd5ab10549a26
Nabelaldwese <Nbelaldwese1@gmail.com>
2026-02-03 09:12:48 +0000

    feat

---
+++ javascript-dom_manipulation/4-script.js
@@ -0,0 +1,6 @@
+#!/usr/bin/node
+document.querySelector('#add_item').addEventListener('click', () => {
+  const newItem = document.createElement('li');
+  newItem.textContent = 'Item';
+  document.querySelector('.my_list').appendChild(newItem);
+});
