8cf279d2e3ac192c862baa358d09db600cfbed2d
Nabelaldwese <Nbelaldwese1@gmail.com>
2026-02-03 09:32:17 +0000

    feat

---
+++ python-server_side_rendering/task_01_jinja.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""Task 1: Creating a Basic HTML Template in Flask (Jinja includes)."""
+
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def home():
+    """Home page."""
+    return render_template('index.html')
+
+
+@app.route('/about', strict_slashes=False)
+def about():
+    """About page."""
+    return render_template('about.html')
+
+
+@app.route('/contact', strict_slashes=False)
+def contact():
+    """Contact page."""
+    return render_template('contact.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True, port=5000)
